63d6afd42b1d70975edcbd07ebc33a4653fb2b15
Frankline Sable <franklineodero@gmail.com>
2022-01-13 11:46:47 +0300

    bug fixes backed

--- lib/main.dart
+++ lib/main.dart
@@ -33,25 +33,37 @@ class QuizAppState extends State {
     return Scaffold(
       appBar: AppBar(title: const Text("Simple Quiz App")),
       body: Center(
-          child: Column(
-        children: [
-          SizedBox(height: 16),
-          Text(
-            _questions[_questionIndex]['question'] as String,
-            style: TextStyle(fontSize: 24),
-          ),
-          SizedBox(height: 16),
-          ...(_questions[_questionIndex]['answers'] as List<String>).map((answer){
-            return Column(
-              children: [
-                ElevatedButton(
-                    onPressed: () => {_answerQuestion(answer)},
-                    child: Text(answer)),
-                SizedBox(height: 16),
-              ],
-            );
-          }).toList()
-        ],
+          child: _questionIndex<_questions.length?
+          Column(
+            children: [
+              SizedBox(height: 16),
+              Text(
+                _questions[_questionIndex]['question'] as String,
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 16),
+              ...(_questions[_questionIndex]['answers'] as List<String>).map((answer){
+                return Column(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () => {_answerQuestion(answer)},
+                        child: Text(answer)),
+                    SizedBox(height: 16),
+                  ],
+                );
+              }).toList()
+            ],
+          ):
+      Container(
+        height: 200,
+        child: Column(
+            children: [
+              Text("You have run out of questions"),
+              SizedBox(height: 16),
+              TextButton(onPressed: _restartQuiz, child: Text('Restart Quiz'))
+            ],
+
+        ),
       )),
     );
   }
@@ -61,5 +73,10 @@ class QuizAppState extends State {
       _questionIndex++;
     });
     print("Answer $answer");
+  }
+  void _restartQuiz(){
+    setState(() {
+      _questionIndex=0;
+    });
   }
 }
